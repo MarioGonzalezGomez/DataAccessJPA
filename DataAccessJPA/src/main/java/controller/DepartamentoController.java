0bf9029650663e0418ed5a428d26fca99d204fd8
MarioGG <ggperpi@gmail.com>
2021-12-14 07:48:44 +0100

    Controller y eliminado de importaciones innecesarias

--- DataAccessJPA/src/main/java/controller/DepartamentoController.java
+++ DataAccessJPA/src/main/java/controller/DepartamentoController.java
@@ -1,6 +1,76 @@
 package controller;
 
+import dto.DepartamentoDTO;
+import repository.DepartamentoRepository;
+import service.DepartamentoService;
+
+
+import java.sql.SQLException;
+import java.util.List;
+import java.util.UUID;
+
 public class DepartamentoController {
+    private static DepartamentoController controller = null;
+
+    // Mi Servicio unido al repositorio
+    private final DepartamentoService departamentoService;
+
+    // Implementamos nuestro Singleton para el controlador
+    private DepartamentoController(DepartamentoService departamentoService) {
+        this.departamentoService = departamentoService;
+    }
+
+    public static DepartamentoController getInstance() {
+        if (controller == null) {
+            controller = new DepartamentoController(new DepartamentoService(new DepartamentoRepository()));
+        }
+        return controller;
+    }
+
+    public List<DepartamentoDTO> getAllDepartamentos() {
+        try {
+            return departamentoService.getAllDepartamentos();
+        } catch (SQLException e) {
+            System.err.println("Error DepartamentoController en getAllDepartamentos: " + e.getMessage());
+            return null;
+        }
+    }
+
+    public DepartamentoDTO getDepartamentoById(UUID id) {
+        try {
+            return departamentoService.getDepartamentoById(id);
+        } catch (SQLException e) {
+            System.err.println("Error DepartamentoController en getDepartamentoById: " + e.getMessage());
+            return null;
+        }
+    }
+
+    public DepartamentoDTO postDepartamento(DepartamentoDTO departamentoDTO) {
+        try {
+            return departamentoService.postDepartamento(departamentoDTO);
+        } catch (SQLException e) {
+            System.err.println("Error DepartamentoController en postDepartamento: " + e.getMessage());
+            return null;
+        }
+    }
+
+    public DepartamentoDTO updateDepartamento(DepartamentoDTO departamentoDTO) {
+        try {
+            return departamentoService.updateDepartamento(departamentoDTO);
+        } catch (SQLException e) {
+            System.err.println("Error DepartamentoController en updateDepartamento: " + e.getMessage());
+            return null;
+        }
+    }
+
+    public DepartamentoDTO deleteDepartamento(DepartamentoDTO departamentoDTO) {
+        try {
+            return departamentoService.deleteDepartamento(departamentoDTO);
+        } catch (SQLException e) {
+            System.err.println("Error DepartamentoController en deleteDepartamento: " + e.getMessage());
+            return null;
+        }
+    }
 
 
 }
--- DataAccessJPA/src/main/java/controller/ProgramadorController.java
+++ DataAccessJPA/src/main/java/controller/ProgramadorController.java
@@ -1,7 +1,75 @@
 package controller;
 
+import dto.ProgramadorDTO;
+import repository.ProgramadorRepository;
+import service.ProgramadorService;
+
+import java.sql.SQLException;
+import java.util.List;
+import java.util.UUID;
+
+
 public class ProgramadorController {
+    private static ProgramadorController controller = null;
 
+    // Mi Servicio unido al repositorio
+    private final ProgramadorService programadorService;
 
+    // Implementamos nuestro Singleton para el controlador
+    private ProgramadorController(ProgramadorService programadorService) {
+        this.programadorService = programadorService;
+    }
+
+    public static ProgramadorController getInstance() {
+        if (controller == null) {
+            controller = new ProgramadorController(new ProgramadorService(new ProgramadorRepository()));
+        }
+        return controller;
+    }
+
+    public List<ProgramadorDTO> getAllProgramadores() {
+        try {
+            return programadorService.getAllProgramadores();
+        } catch (SQLException e) {
+            System.err.println("Error ProgramadorController en getAllProgramadores: " + e.getMessage());
+            return null;
+        }
+    }
+
+    public ProgramadorDTO getProgramadorById(UUID id) {
+        try {
+            return programadorService.getProgramadorById(id);
+        } catch (SQLException e) {
+            System.err.println("Error ProgramadorController en getProgramadorById: " + e.getMessage());
+            return null;
+        }
+    }
+
+    public ProgramadorDTO postProgramador(ProgramadorDTO programadorDTO) {
+        try {
+            return programadorService.postProgramador(programadorDTO);
+        } catch (SQLException e) {
+            System.err.println("Error ProgramadorController en postProgramador: " + e.getMessage());
+            return null;
+        }
+    }
+
+    public ProgramadorDTO updateProgramador(ProgramadorDTO programadorDTO) {
+        try {
+            return programadorService.updateProgramador(programadorDTO);
+        } catch (SQLException e) {
+            System.err.println("Error ProyectoController en updateCategory: " + e.getMessage());
+            return null;
+        }
+    }
+
+    public ProgramadorDTO deleteProyecto(ProgramadorDTO programadorDTO) {
+        try {
+            return programadorService.deleteProgramador(programadorDTO);
+        } catch (SQLException e) {
+            System.err.println("Error ProyectoController en deleteCategory: " + e.getMessage());
+            return null;
+        }
+    }
 
 }
